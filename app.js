bc25e578a3142d13711e97a8baef96a07e7bd59a
Hyun Kong <konghyunsik7@gmail.com>
2022-08-11 00:02:08 -0400

    Updated Smallest websize + Hamburger Bar

--- app.js
+++ app.js
@@ -64,6 +64,21 @@ $(document).ready(function(){
             }
         }
     });
+
+    // Hamburger menu-btn
+
+        const hamburger = document.querySelector(".menu-list");
+        const navMenu = document.querySelector(".menu");
+    
+        hamburger.addEventListener("click", () => {
+            hamburger.classList.toggle("active");
+            navMenu.classList.toggle("active");
+        })
+    
+        document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
+            hamburger.classList.remove("active");  
+            navMenu.classList.remove("active");  
+        }))
  
 
 });
